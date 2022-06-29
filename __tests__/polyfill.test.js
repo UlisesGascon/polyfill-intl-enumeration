bf3019e193cadb3f5d51880b276a56a175706f11
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-06-29 12:30:22 +0200

    test: added basic testing TODOs

---
+++ __tests__/polyfill.test.js
@@ -0,0 +1,14 @@
+describe('Polyfill load', () => {
+  it.todo('Should ADD the polyfill in the global scope')
+})
+
+describe('Intl.supportedValuesOf', () => {
+  it.todo('Should trow RangeError if the key is empty')
+  it.todo('Should trow RangeError if the key is invalid')
+  it.todo('Should return calendar list')
+  it.todo('Should return collation list')
+  it.todo('Should return currency list')
+  it.todo('Should return numberingSystem list')
+  it.todo('Should return timeZone list')
+  it.todo('Should return unit list')
+})
